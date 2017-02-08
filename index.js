ed3f5d8b38cc1a32cfb8768a1156e8f79a0a02cd
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-08 20:46:39 +0100

    prints json

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import Koa from 'koa'
 import Router from 'koa-router'
+import json from 'koa-json'
 import db from './utils/mongo'
 import main from './routes/main'
 const app = new Koa()
 const router = new Router()
 const PORT = 3000
 
-router.use('/', main.routes())
+app.use(json())
 
+router.use('/', main.routes())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,18 @@
 import Router from 'koa-router'
 const router = new Router()
 
-router.get('/', async (ctx, next) => {
-  ctx.body = 'main'
-})
+router
+  .get('/', async (ctx, next) => {
+    ctx.status = 200
+
+    ctx.body = {
+      id: 0,
+      username: "Northerntwig",
+      name: "Oscar Nordquist",
+      posts: "http://localhost:3000/posts"
+    }
+
+    console.log(ctx)
+  })
 
 export default router
