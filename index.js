256203648ac6bb5656aea2ae08af10b2149b728c
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-08 19:20:37 +0100

    router init

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import Koa from 'koa'
+import Router from 'koa-router'
+import main from './routes/main'
 const app = new Koa()
+const router = new Router()
 const PORT = 3000
 
+router.use('/', main.routes())
 
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
