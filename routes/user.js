d51eac831400e5237cdd0de8791f9646aeb9fb2d
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-16 20:07:14 +0100

    can get single user from id

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,9 @@ router
   .get('users', async (ctx, next) => {
     ctx.body = await userSchema.find({}, 'id username name', {lean: true})
   })
-  .get('users/:id', async (ctx, next) => {
-    ctx.body = userSchema.find({}, 'id username name')
+  .get('users/:_id', async (ctx, next) => {
+    const { _id } = ctx.params
+    ctx.body = await userSchema.find({_id}, 'id username name')
   })
   .post('users', createUserCheck, async (ctx, next) => {
     const { username, password, name } = ctx.request.body
