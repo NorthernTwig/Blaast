f88a4e18998e67095cdd1aaa45fb5db9545e8a26
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-18 12:21:24 +0100

    pagination for comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,13 +1,24 @@
 import Router from 'koa-router'
+import CommentSchema from '../models/schemas/CommentSchema'
+import pagination from '../utils/pagination'
 const router = new Router()
+
 
 router
   .get('comments', async (ctx, next) => {
-    ctx.body = 'comments'
+    const limit = parseInt(ctx.query.limit) || 10
+    const offset = parseInt(ctx.query.offset) || 0
+    const path = ctx.req._parsedUrl.pathname
+    
+    const comments = CommentSchema.find({}, '_id author body date', {lean: true})
+        .sort({ 'date': -1 })
+        .limit(limit)
+        .skip(offset * limit)
+
+    ctx.body = pagination(comments, ctx.url, limit, offset, path)
   })
   .get('comments/:_id', async (ctx, next) => {
-    
-  })
 
+  })
 
 export default router
