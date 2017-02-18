f0f8698be69c95ba192471cde05bea7e9a91a388
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-18 13:01:00 +0100

    getting seperate comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router'
 import CommentSchema from '../models/schemas/CommentSchema'
+import jwt from './middlewares/jwt'
 import pagination from '../utils/pagination'
 const router = new Router()
 
@@ -10,7 +11,7 @@ router
     const offset = parseInt(ctx.query.offset) || 0
     const path = ctx.req._parsedUrl.pathname
     
-    const comments = CommentSchema.find({}, '_id author body date', {lean: true})
+    const comments = CommentSchema.find({}, '_id body author post date', { lean: true })
         .sort({ 'date': -1 })
         .limit(limit)
         .skip(offset * limit)
@@ -18,7 +19,15 @@ router
     ctx.body = pagination(comments, ctx.url, limit, offset, path)
   })
   .get('comments/:_id', async (ctx, next) => {
-
+    const { _id } = ctx.params
+    const comment = CommentSchema.findOne({ _id }, '_id body author post date', { lean: true })
+    ctx.body = comments  
   })
+  .post('comments', jwt, async (ctx, next) => {
+    const { body, post } = ctx.request.body
+
+    
+
+  })  
 
 export default router
