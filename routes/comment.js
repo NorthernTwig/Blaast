ca9618f4b05918f732328c93f8f0348c2fe22b2f
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-18 16:47:38 +0100

    added self to comments

--- routes/comment.js
+++ routes/comment.js
@@ -2,6 +2,7 @@ import Router from 'koa-router'
 import CommentSchema from '../models/schemas/CommentSchema'
 import jwt from './middlewares/jwt'
 import pagination from '../utils/pagination'
+import domain from '../utils/domain'
 const router = new Router()
 
 
@@ -11,17 +12,32 @@ router
     const offset = parseInt(ctx.query.offset) || 0
     const path = ctx.req._parsedUrl.pathname
     
-    const comments = await CommentSchema.find({}, '_id body author postId date', { lean: true })
+    let comments = await CommentSchema.find({}, '_id body author post date', { lean: true })
         .sort({ 'date': -1 })
         .limit(limit)
         .skip(offset * limit)
+
+    comments = comments.map(comment => {
+      return 
+    })
 
     ctx.body = pagination(comments, ctx.url, limit, offset, path)
   })
   .get('comments/:_id', async (ctx, next) => {
     const { _id } = ctx.params
-    const comment = CommentSchema.findOne({ _id }, '_id body author post date', { lean: true })
-    ctx.body = comments
+    const comment = await CommentSchema.findOne({ _id }, '_id body author post date', { lean: true })
+
+    ctx.body = Object.assign(comment, {
+      post: {
+        _id: comment.post,
+        self: `${ domain() }/posts/${ comment.post }`
+      },
+      author: Object.assign(comment.author, {
+        self: `${ domain() }/users/${ comment.author._id }`
+      }),
+      self: `${ domain() }${ ctx.url }`
+    })
+
   })
   .post('comments', jwt, async (ctx, next) => {
     const { body, post } = ctx.request.body
