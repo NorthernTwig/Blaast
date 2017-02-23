453f85ea2447f2e7abee25e4c8c68bd1330323e9
Oscar Nordquist <on222bx@student.lnu.se>
2017-02-23 22:08:31 +0100

    accidently wrote touch

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ echo 'Dependencies installed'
 
 echo 'Creating .env file'
 
-touch 'DB_HOST=mongodb://localhost/blog-mongo
+echo 'DB_HOST=mongodb://localhost/blog-mongo
 PUBLIC_SECRET=youshouldchangethis1'>>.env
 
 echo 'Done.'
